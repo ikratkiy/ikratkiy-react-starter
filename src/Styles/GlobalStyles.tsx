5fb1c58c9ee5a3c33d45aea593c4371a8e5578fb
ikratkiy <ivan.bryukhov@gmail.com>
2023-03-06 14:51:49 +0300

    added root css variables

--- src/Styles/GlobalStyles.tsx
+++ src/Styles/GlobalStyles.tsx
@@ -1,12 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-    body {
-        background-color: #eee;
-        color: #444;
+    :root {
+        --background: #eee;
+        --text: #444;
+
         @media (prefers-color-scheme: dark) {
-            background-color: #444;
-            color: #eee;
+            --background: #141414;
+            --text: #eee;
         }
+    }
+
+    body {
+        background-color: var(--background);
+        color: var(--text);
     }
 `;
