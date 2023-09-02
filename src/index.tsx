81c58c1abe33ffa4476d7027f85e4fcbffae45d6
ikratkiy <ivan.bryukhov@gmail.com>
2023-09-02 06:00:26 +0300

    обновил рендер для Реакт 18 и добавил структурные папки

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,24 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+const container = document.getElementById('app');
+const root = createRoot(container!);
+
+root.render(
     <StrictMode>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </StrictMode>,
-    document.getElementById('app')
+    </StrictMode>
 );
+
+// ReactDOM.render(
+//     <StrictMode>
+//         <BrowserRouter>
+//             <App />
+//         </BrowserRouter>
+//     </StrictMode>,
+//     document.getElementById('app')
+// );
